765baa717b385284cbed350f5475b26b0372c326
odiegofarias <diego.farias.fernandes@gmail.com>
2022-07-11 19:53:42 -0300

    mudando configs pesssoais

--- webdev/settings.py
+++ webdev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8*d-97!+n0tm-8jv2lxipc%=x_7vx(uw&_1yldn#xx8r1hi(cn'
+SECRET_KEY = config('SECRET_KEY')
 #
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
